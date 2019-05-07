bc2eac92cbe21f4100dc24f33849c37545451c66
Derek Townsend <derekjavon2@gmail.com>
2019-05-07 00:23:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,10 +32,9 @@ end
 
 def initial_round
   # code #initial_round here
-  2.times do
-    card=deal_card
-    card_total+=card
-  end
+  card1=deal_card
+  card2=deal_card
+  card_total=card1+card2
   display_card_total(card_total)
   return card_total
 end
